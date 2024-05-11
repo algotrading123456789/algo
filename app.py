631dd3d65800bef9d7e34129baf7d873030fc027
algotrading123456789 <aimdse3@gmail.com>
2024-05-12 01:53:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import pytz
 from datetime import datetime
-
+st.title('Welcome to Option Maniancs')
 class vix_india:
     def __init__(self, starting_date, ending_date=None):
         self.starting_date = starting_date
