16b372d76c025213ea7bbf1419276bb5a6af1c5b
algotrading123456789 <aimdse3@gmail.com>
2024-05-12 15:38:36 +0530

    Update Exp.py

--- Exp.py
+++ Exp.py
@@ -342,12 +342,4 @@ def job():
     if message:
         st.title(f"{message}")
 
-# Schedule the job to run every 5 minutes
-schedule.every(5).minutes.do(job)
 
-# Infinite loop to run the scheduler
-while True:
-    schedule.run_pending()
-    time.sleep(200)
-
-job()
